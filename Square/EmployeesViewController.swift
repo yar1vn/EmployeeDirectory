7d690eb5bc216dd5f38924fe67ebe0c368424b12
Yariv Nissim <yariv.nis@me.com>
2021-03-16 18:30:27 -0700

    Rename ViewController -> EmployeesViewController
    Organize folders

---
+++ Square/EmployeesViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Square
+//
+//  Created by Yariv Nissim on 3/16/21.
+//
+
+import UIKit
+
+/*
+ TODO:
+ - Model + ViewModel
+ - Network layer
+ - Unit Tests
+ - Image Cache
+ - Basic UI - Collection View + Diffable
+ - Customize UI - Rounded Corners, Shadows, ..
+ */
+
+class EmployeesViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+}
+
